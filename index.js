a04d6d4335118decb7c15f327173682796134ea3
MZaharov <beaviz0405@gmail.com>
2020-07-04 20:41:59 +0300

    Упрощение метода прохода по узлам

--- index.js
+++ index.js
@@ -11,49 +11,58 @@
       this.input = this.setElement(input)
       this.template = this.setElement(template)
       this.savedContent = this.template.innerHTML;
-      this.onChange = debounce(this.search.bind(this));
+      this.debouncedSearch = debounce(this.search);
 
       this.init();
     }
-    search(e) {
+    search = (e) => {
       const { value } = e.target
       this.template.innerHTML = this.savedContent;
       if (value) {
         const regx = new RegExp(value, 'ig');
-        const matches = this.findMatchesRange(regx, this.template);
-        if(matches.length) this.render(matches);
+        const matches = this.findMatchesRange(regx, this.walkNodes(this.template));
+        if (matches.length) this.render(matches);
       }
     }
-    findMatchesRange(regx, template) {
+        
+    findMatchesRange = (regx, nodesGenerator) => {
       const allRanges = [];
-      template.childNodes.forEach((node) => {
-        const { nodeType, nodeValue, childNodes } = node;
+      for (let node of nodesGenerator) {
+        const { nodeType, nodeValue } = node;
         const isTextNode = nodeType === 3;
-        const textNodeNotEmpty = isTextNode && nodeValue.trim().replace(/\s/g, '').length
-        if (textNodeNotEmpty) {
+        if (isTextNode && this.checkStringNotEmpty(nodeValue)) {
           const matches = [...nodeValue.matchAll(regx)];
-          if (matches.length) {
-            matches.forEach(match => {
-              const range = document.createRange();
-              const { index } = match;
-              const searchLength = match[0].length;
-              range.setStart(node, index);
-              range.setEnd(node, index + searchLength);
-              allRanges.push(range)
-            });
-          }
+          if (matches.length) allRanges.push(...this.createRangesByMatches(node, matches))
         }
-        if (childNodes) allRanges.push(...this.findMatchesRange(regx, node));
-      })
+      }
       return allRanges;
     }
-    setElement(el) {
-      return typeof el === 'string' ? document.querySelector(el) : el;
+    
+    *walkNodes (node) {  
+      yield node;
+      let element = node.firstChild;
+      while(element) {
+        yield* this.walkNodes(element)
+        element = element.nextSibling
+      }
     }
-    init() {
-      this.input.addEventListener('input', this.onChange)
-    }
-    render(matchesRange) {
+    
+    createRangesByMatches = (node, matches) => matches.map((match) => {
+      const range = document.createRange();
+      const { index } = match;
+      const searchLength = match[0].length;
+      range.setStart(node, index);
+      range.setEnd(node, index + searchLength);
+      return range
+    })
+    
+    checkStringNotEmpty = (nodeValue) => nodeValue.trim().replace(/\s/g, '').length > 0
+    
+    setElement = (el) => typeof el === 'string' ? document.querySelector(el) : el;
+    
+    init = () => this.input.addEventListener('input', this.debouncedSearch)
+    
+    render = (matchesRange) => {
       matchesRange.forEach((range) => {
         const mark = document.createElement('mark');
         range.surroundContents(mark);
